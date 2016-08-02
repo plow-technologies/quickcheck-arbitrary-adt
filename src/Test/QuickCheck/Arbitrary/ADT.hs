07a51c17282baa6a6cb401a9cf4c8ba565a47a86
James Haver <mchaver@gmail.com>
2016-08-03 00:33:29 +0800

    Remove unused code

--- src/Test/QuickCheck/Arbitrary/ADT.hs
+++ src/Test/QuickCheck/Arbitrary/ADT.hs
@@ -1,48 +1,34 @@
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE TypeOperators     #-}
-{-# LANGUAGE TypeFamilies #-}
-
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE UndecidableInstances #-}
-
-{-# LANGUAGE ScopedTypeVariables #-}
-
-{-# LANGUAGE DefaultSignatures   #-}
-
 {-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE ExistentialQuantification, RankNTypes #-}
+{-# LANGUAGE DefaultSignatures   #-}
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators     #-}
 
 module Test.QuickCheck.Arbitrary.ADT where
+
+import           Data.Typeable
 
 import           GHC.Generics
 
 import           Safe (headMay)
 
-import           Test.QuickCheck.Gen
 import           Test.QuickCheck.Arbitrary
+import           Test.QuickCheck.Gen
 
-import           Data.Typeable
 
--- | ToArbitraryConstructor type class provides to functions.
--- `toArbitraryConstructor` creates an arbitrary instance of one of the
+
+-- | ToADTArbitrary type class provides two functions.
+-- `toADTArbitrarySingleton` creates an arbitrary instance of one of the
 -- constructors of a given type.
--- `toArbitraryConstructorList` creates an arbitrary instance of each of the
+-- `toADTArbitrary` creates an arbitrary instance of each of the
 -- constructors of a given type.
 -- If you use the default implementation with generic derivation,
--- `toArbitraryConstructorList` will automatically provide an arbitrary for
+-- `toADTArbitrary` will automatically provide an arbitrary for
 -- each constructor.
 -- If you define it yourself, it is your responsibility to make sure that
 -- an arbitrary instance of each type constructor is provided. The type signature
--- does not guarantee that there will an instance of each type constructor.
-
-class ToArbitraryConstructor a where
-  toArbitraryConstructor :: Gen (String,a)
-  default toArbitraryConstructor :: (Generic a, GToArbitraryConstructor (Rep a)) => Gen (String, a)
-  toArbitraryConstructor = fmap to <$> gToArbitraryConstructor
-
-  toArbitraryConstructorList :: Gen [(String,a)]
-  default toArbitraryConstructorList :: (Generic a, GToArbitraryConstructorList (Rep a)) => Gen [(String, a)]
-  toArbitraryConstructorList = (fmap . fmap) to <$> gToArbitraryConstructorList
+-- does not guarantee that there will be an instance of each type constructor.
 
 class ToADTArbitrary a where
   toADTArbitrarySingleton :: Proxy a -> Gen (ADTArbitrarySingleton a)
@@ -52,7 +38,7 @@ class ToADTArbitrary a where
                                     , GToADTArbitrarySingleton (Rep (ADTArbitrarySingleton a)))
                                     => Proxy a
                                     -> Gen (ADTArbitrarySingleton a)
-  toADTArbitrarySingleton = genericToADTArbitrarySingleton
+  toADTArbitrarySingleton _ = fmap to <$> gToADTArbitrarySingleton (Proxy :: Proxy (Rep a))
 
   toADTArbitrary :: Proxy a -> Gen (ADTArbitrary a)
   default toADTArbitrary :: ( Arbitrary a
@@ -61,34 +47,8 @@ class ToADTArbitrary a where
                             , GToADTArbitrary (Rep (ADTArbitrary a)))
                            => Proxy a
                            -> Gen (ADTArbitrary a)
-  toADTArbitrary = genericToADTArbitrary
+  toADTArbitrary _ = fmap to <$> gToADTArbitrary (Proxy :: Proxy (Rep a))
 
-  --toADTArbitrary :: Proxy a -> Gen (ADTArbitrary a)
-  --default toADTArbitrary :: (Generic a, GToArbitraryConstructorList (Rep a)) => Gen [(String, a)]
-  --toADTArbitrary = (fmap . fmap) to <$> gToArbitraryConstructorList
-
-
-{-
-genericToADTArbitrary :: forall a.
-                        ( Arbitrary a
-                        , Generic a
-                        , GToADTArbitrary (Rep a)
-                        , GToADTArbitrary (Rep (ADTArbitrary a)))
-                       => Proxy a
-                       -> Gen (ADTArbitrary a)
-genericToADTArbitrary _ = fmap to <$> gToADTArbitrary (Proxy :: Proxy (Rep a))
-
-
-genericToADTArbitrarySingleton :: forall a.
-                                  ( Arbitrary a
-                                  , Generic a
-                                  , GToADTArbitrarySingleton (Rep a)
-                                  , GToADTArbitrarySingleton (Rep (ADTArbitrarySingleton a)))
-                                 => Proxy a
-                                 -> Gen (ADTArbitrarySingleton a)
-genericToADTArbitrarySingleton _ = fmap to <$> gToADTArbitrarySingleton (Proxy :: Proxy (Rep a))
-
--}
 
 
 
@@ -121,93 +81,6 @@ instance GArbitrary rep => GArbitrary (M1 i t rep) where
 
 genericArbitrary :: (Generic a, GArbitrary (Rep a)) => Gen a
 genericArbitrary = to <$> gArbitrary
-
-
-
--- | GToArbitraryConstructor creates an arbitrary and returns the name of the
--- constructor that was used to create it
-
-class GToArbitraryConstructor rep where
-  gToArbitraryConstructor :: Gen (String, rep a)
-
-instance GToArbitraryConstructor U1 where
-  gToArbitraryConstructor = pure ("",U1)
-
-instance (GToArbitraryConstructor l, GToArbitraryConstructor r) => GToArbitraryConstructor (l :+: r) where
-  gToArbitraryConstructor = do
-    b <- arbitrary
-    if b then fmap L1 <$> gToArbitraryConstructor
-         else fmap R1 <$> gToArbitraryConstructor
-
-instance (GToArbitraryConstructor l, GToArbitraryConstructor r) => GToArbitraryConstructor (l :*: r) where
-  gToArbitraryConstructor = do
-    (_,x) <- gToArbitraryConstructor
-    (_,y) <- gToArbitraryConstructor
-    return ("", x :*: y)
-
-instance Arbitrary a => GToArbitraryConstructor (K1 i a) where
-  gToArbitraryConstructor = (,) "" . K1 <$> arbitrary
-
-instance (Constructor c, GToArbitraryConstructor rep) => GToArbitraryConstructor (M1 C c rep) where
-  gToArbitraryConstructor = fmap M1 <$> ((,) <$> pure con <*> (snd <$> gToArbitraryConstructor))
-    where
-      con = conName (undefined :: M1 C c rep ())
-
-instance GToArbitraryConstructor rep => GToArbitraryConstructor (M1 D t rep) where
-  gToArbitraryConstructor = fmap M1 <$> gToArbitraryConstructor
-
-instance GToArbitraryConstructor rep => GToArbitraryConstructor (M1 S t rep) where
-  gToArbitraryConstructor = fmap M1 <$> gToArbitraryConstructor
-
-
--- | GToArbitraryConstructorList is a typeclass for generalizing the creation
--- of a list of arbitrary instances of each constructor of a type.  It also
--- includes the name of the constructor as a String for reference and file
--- creation.
-
-class GToArbitraryConstructorList rep where
-  gToArbitraryConstructorList :: Gen [(String, rep a)]
-
-instance GToArbitraryConstructorList U1 where
-  gToArbitraryConstructorList = pure [("",U1)]
-
-instance (GToArbitraryConstructorList l, GToArbitraryConstructorList r) => GToArbitraryConstructorList (l :+: r) where
-  gToArbitraryConstructorList = (++) <$> ((fmap . fmap) L1 <$> gToArbitraryConstructorList) <*> ((fmap . fmap) R1 <$> gToArbitraryConstructorList)
-
-instance (GToArbitraryConstructor l, GToArbitraryConstructorList l, GToArbitraryConstructor r, GToArbitraryConstructorList r) => GToArbitraryConstructorList (l :*: r) where
-  gToArbitraryConstructorList = do
-    xs <- gToArbitraryConstructor
-    ys <- gToArbitraryConstructor
-    return [("", snd xs :*: snd ys)]
-
-{-
-instance (Constructor c, GToArbitraryConstructorList rep) => GToArbitraryConstructorList (M1 C c rep) where
-  gToArbitraryConstructorList = (fmap . fmap) M1 . (:[]) <$> ((,) <$> pure con <*> (snd . head <$> gToArbitraryConstructorList))
-    where
-      con = conName (undefined :: M1 C c rep ())
--}
-
-instance (Constructor c, GToArbitraryConstructorList rep) => GToArbitraryConstructorList (M1 C c rep) where
-  gToArbitraryConstructorList = do
-    mConRep <- fmap snd . headMay <$> gToArbitraryConstructorList
-    case mConRep of
-      Nothing     -> fail "GToArbitraryConstructorList constructor representation returned an empty list. This should not happen."
-      Just conRep -> return $  fmap M1 <$> [(conString, conRep)]
-    where
-      conString = conName (undefined :: M1 C c rep ())
-
-
-instance GToArbitraryConstructorList rep => GToArbitraryConstructorList (M1 D t rep) where
-  gToArbitraryConstructorList = (fmap . fmap) M1 <$> gToArbitraryConstructorList
-
-instance GToArbitraryConstructorList rep => GToArbitraryConstructorList (M1 S t rep) where
-  gToArbitraryConstructorList = (fmap . fmap) M1 <$> gToArbitraryConstructorList
-
--- | at the Rep level there is no access to the constructor name
--- pass the arbitrary for the constructor with an empty string
--- the empty string will be replaced at the declaration for `M1 C c rep`
-instance Arbitrary a => GToArbitraryConstructorList (K1 i a) where
-  gToArbitraryConstructorList = (:[]) . (,) "" . K1 <$> arbitrary
 
 
 
@@ -257,14 +130,9 @@ instance (Arbitrary a) => Arbitrary (ADTArbitrary a) where
 
 
 
-genericToADTArbitrarySingleton :: forall a.
-                                  ( Arbitrary a
-                                  , Generic a
-                                  , GToADTArbitrarySingleton (Rep a)
-                                  , GToADTArbitrarySingleton (Rep (ADTArbitrarySingleton a)))
-                                 => Proxy a
-                                 -> Gen (ADTArbitrarySingleton a)
-genericToADTArbitrarySingleton _ = fmap to <$> gToADTArbitrarySingleton (Proxy :: Proxy (Rep a))
+-- | GToADTArbitrarySingleton creates an arbitrary and returns the name of the
+-- constructor that was used to create it and the type name.
+
 
 class GToADTArbitrarySingleton rep where
   gToADTArbitrarySingleton :: Proxy rep -> Gen (ADTArbitrarySingleton (rep a))
@@ -326,20 +194,10 @@ instance GToADTArbitrarySingleton rep => GToADTArbitrarySingleton (M1 S t rep) w
 
 
 
-
-
-
-
-
-
-genericToADTArbitrary :: forall a.
-                        ( Arbitrary a
-                        , Generic a
-                        , GToADTArbitrary (Rep a)
-                        , GToADTArbitrary (Rep (ADTArbitrary a)))
-                       => Proxy a
-                       -> Gen (ADTArbitrary a)
-genericToADTArbitrary _ = fmap to <$> gToADTArbitrary (Proxy :: Proxy (Rep a))
+-- | GToADTArbitrary is a typeclass for generalizing the creation
+-- of a list of arbitrary instances of each constructor of a type.  It also
+-- returns the name of the constructor and the type name for reference and file
+-- creation.
 
 class GToADTArbitrary rep where
   gToADTArbitrary :: Proxy rep -> Gen (ADTArbitrary (rep a))
